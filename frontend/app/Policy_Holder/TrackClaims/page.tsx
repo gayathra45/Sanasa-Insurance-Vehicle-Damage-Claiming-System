35f888d3597a2c70bb6440bcfb0b23316431f0d5
Gayathra Samuditha Vithanage Nanayakkara <gayathrasamuditha45@gmail.com>
2026-07-20 00:46:00 +0530

    update

--- frontend/app/Policy_Holder/TrackClaims/page.tsx
+++ frontend/app/Policy_Holder/TrackClaims/page.tsx
@@ -636,7 +636,7 @@ function TrackClaimsContent() {
               <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5.586a1 1 0 0 1 .707.293l5.414 5.414a1 1 0 0 1 .293.707V19a2 2 0 0 1-2 2z" />
             </svg>
             Enter your Claim ID in the search bar above to track your claim&apos;s status.
-          </div>
+         </div>
         )}
 
       </main>
